0111b443fdc5c0c266aee2ac93adc2dabe0d83a3
Iker Rico <ikervrico257@gmail.com>
2024-05-03 01:45:00 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import { RouterView } from 'vue-router'
+
+export default {
+  components: {
+    RouterView
+  }
+}
+</script>
+<template>
+  <div id="app" class="container-fluid">
+    <RouterView />
+  </div>
+</template>
+
+<style>
+html {
+  background-color: #929292;
+  height: 100%;
+  font-family: 'Open sans';
+}
+
+#app {
+  height: 100%;
+  font-family: 'Open sans';
+}
+</style>
---
+++ src/views/homeView.vue
@@ -0,0 +1,51 @@
+<script></script>
+<template>
+  <div class="contenedor d-flex flex-column justify-content-center border border-white border-5">
+    <div class="title-selector row text-center">
+      <div class="section-selector col">
+        <h1>Elije una opción</h1>
+      </div>
+    </div>
+    <div class="buttons-section row text-center">
+      <div class="button-selector col">
+        <button type="button" class="btn btn-primary btn-lg">
+          <i class="bi bi-cart"></i>
+          <span>Administración</span>
+        </button>
+      </div>
+      <div class="button-selector col">
+        <button type="button" class="btn btn-primary btn-lg">
+          <i class="bi bi-truck"></i>
+          <span>Producción</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.contenedor {
+  height: 100vh;
+  background-color: #929292;
+  border-color: #fff;
+}
+
+.buttons-section .button-selector button {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 10px;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+
+.buttons-section .button-selector i {
+  font-size: 7rem;
+}
+
+.buttons-section .button-selector span {
+  font-size: 2rem;
+}
+</style>
