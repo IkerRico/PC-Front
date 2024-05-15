39302a35ed13523f24dfa1bee1d096513a3e081c
Iker Rico <ikervrico257@gmail.com>
2024-05-15 22:08:32 +0200

    change base url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "https://ikerrico.github.io/PC-Front/"
+  base: 'https://ikerrico.github.io/PC-Front',
   server: {
     watch: {
       usePolling: true
