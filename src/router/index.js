5e953bf224d482aa09980f3fe1cb2b9db7f3edbf
Iker Rico <ikervrico257@gmail.com>
2024-05-17 23:18:54 +0200

    quitamos redirect

--- src/router/index.js
+++ src/router/index.js
@@ -33,10 +33,6 @@ const router = createRouter({
       name: 'Articulos',
       component: () =>
         import('../views/Administracion/Articulos/GestionArticulos/articulosMain.vue')
-    },
-    {
-      path: '/:pathMatch(.*)',
-      redirect: '/'
     }
   ]
 })
