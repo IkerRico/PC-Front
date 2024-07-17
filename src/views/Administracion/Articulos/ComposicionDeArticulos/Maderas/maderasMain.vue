17d46df39bc4d9954fb058ac2cb0ed3eba8c2a56
Iker Rico <ikervrico257@gmail.com>
2024-07-17 22:47:22 +0200

    correccion mensaje carga maderas

--- src/views/Administracion/Articulos/ComposicionDeArticulos/Maderas/maderasMain.vue
+++ src/views/Administracion/Articulos/ComposicionDeArticulos/Maderas/maderasMain.vue
@@ -1,63 +1,60 @@
 <script>
-import NavbarAdministracion from "@/components/NavbarAdministracion.vue";
-import TituloDeMenu from "@/components/TituloDeMenu.vue";
+import NavbarAdministracion from '@/components/NavbarAdministracion.vue'
+import TituloDeMenu from '@/components/TituloDeMenu.vue'
 
-import { mapActions, mapState } from "pinia";
+import { mapActions, mapState } from 'pinia'
 
-import { maderasStore } from "@/stores/MaderasStore.js";
+import { maderasStore } from '@/stores/MaderasStore.js'
 
 export default {
   components: {
     NavbarAdministracion,
-    TituloDeMenu,
+    TituloDeMenu
   },
   data() {
     return {
       cargarLoader: false,
       columnas: [
-        { title: "Id", width: "5%", align: "center" },
-        { title: "Nombre", width: "50%", align: "left" },
-      ],
-    };
+        { title: 'Id', width: '5%', align: 'center' },
+        { title: 'Nombre', width: '50%', align: 'left' }
+      ]
+    }
   },
   computed: {
     ...mapState(maderasStore, {
-      maderas: "maderas",
-      datosCargados: "datosCargados",
-    }),
+      maderas: 'maderas',
+      datosCargados: 'datosCargados'
+    })
   },
   methods: {
-    ...mapActions(maderasStore, ["obtenerTodasLasMaderas"]),
+    ...mapActions(maderasStore, ['obtenerTodasLasMaderas']),
     async cargarMaderas() {
       if (!this.datosCargados) {
         try {
-          await this.obtenerTodasLasMaderas();
+          await this.obtenerTodasLasMaderas()
         } catch (error) {
           this.$swal({
-            icon: "error",
-            title: "Error",
-            text:
-              "Hubo un error al cargar los artículos. Por favor, inténtalo de nuevo más tarde.",
-          });
+            icon: 'error',
+            title: 'Error',
+            text: 'Hubo un error al cargar las maderas. Por favor, inténtalo de nuevo más tarde.'
+          })
         }
       }
     },
     manejarAccion({ accion, filaData }) {
-      if (accion === "ver") {
-        this.$router.push(
-          `/GestionArticulos/ComposicionArticulos/Maderas/${filaData.id}/ver`
-        );
-      } else if (accion === "editar") {
+      if (accion === 'ver') {
+        this.$router.push(`/GestionArticulos/ComposicionArticulos/Maderas/${filaData.id}/ver`)
+      } else if (accion === 'editar') {
         // Maneja la editar del artículo aquí
-      } else if (accion === "eliminar") {
+      } else if (accion === 'eliminar') {
         // Maneja la eliminación del artículo aquí
       }
-    },
+    }
   },
   mounted() {
-    this.cargarMaderas();
-  },
-};
+    this.cargarMaderas()
+  }
+}
 </script>
 <template>
   <div class="contenedor row p-0 m-0 h-100">
